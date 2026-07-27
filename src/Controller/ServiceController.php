590f3bc3a38c049cbe9351ef607949a8dc6b0f60
alexisdejean <alexis.dejean@campuscarloacutis.fr>
2026-07-27 20:54:18 +0200

    Ajout de fonctionnalités

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class ServiceController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/service', name: 'app_service')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'projet' => 'Symfony Web MarketPlace',
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
         ]);
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_IDENTIFIANT', fields: ['identifiant'])]
+#[UniqueEntity(fields: ['identifiant'], message: 'There is already an account with this identifiant')]
+
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $identifiant = null;
+
+    /**
+        * 0 = utilisateur
+        * 1 = administrateur
+ */
+    #[ORM\Column(options: ['default' => 0])]
+    private int $role = 0;
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private bool $isVerified = true;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdentifiant(): ?string
+    {
+        return $this->identifiant;
+    }
+
+    public function setIdentifiant(string $identifiant): static
+    {
+        $this->identifiant = $identifiant;
+
+        return $this;
+    }
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->identifiant;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+{
+    return match ($this->role) {
+        1 => ['ROLE_ADMIN'],
+        default => ['ROLE_USER'],
+    };
+}
+
+    /**
+ * Retourne le rôle numérique de l'utilisateur
+ * 0 = utilisateur
+ * 1 = administrateur
+ */
+    public function getRole(): int
+{
+    return $this->role;
+}
+
+public function setRole(int $role): static
+{
+    $this->role = $role;
+
+    return $this;
+}
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+
+        return $data;
+    }
+
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+        // @deprecated, to be removed when upgrading to Symfony 8
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+}
